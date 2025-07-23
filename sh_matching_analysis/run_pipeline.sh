f6b7ccf5f461e114ac06c38fafb0cea4d3bb9c10
givdieri <75476306+givdieri@users.noreply.github.com>
2025-07-23 15:25:56 +0200

    sync with upstream updates

--- sh_matching_analysis/run_pipeline.sh
+++ sh_matching_analysis/run_pipeline.sh
@@ -176,22 +176,22 @@ if [ "$include_usearch_05_step" == "yes" ]; then
 
     echo "Running 97% pre-clustering..."
     ## 97% pre-clustering
-    "$program_dir/usearch" -cluster_fast "$user_dir/iupac_out_vsearch.fasta" -id 0.97 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$user_dir/clusters_97_pre.uc"
+    "$program_dir/usearch" -cluster_fast "$user_dir/iupac_out_vsearch.fasta" -id 0.97 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$user_dir/clusters_97_pre.uc"
     python3 "$script_dir/clusterparser_preclust1_pre.py" "$run_id"
 
     echo "Running 95% pre-clustering..."
     ## 95% pre-clustering
-    "$program_dir/usearch" -cluster_fast "$user_dir/in_95_pre.fasta" -id 0.95 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$user_dir/clusters_95_pre.uc"
+    "$program_dir/usearch" -cluster_fast "$user_dir/in_95_pre.fasta" -id 0.95 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$user_dir/clusters_95_pre.uc"
     python3 "$script_dir/clusterparser_preclust2_pre.py" "$run_id"
 
     echo "Running 90% pre-clustering..."
     ## 90% pre-clustering
-    "$program_dir/usearch" -cluster_fast "$user_dir/in_90_pre.fasta" -id 0.90 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$user_dir/clusters_90_pre.uc"
+    "$program_dir/usearch" -cluster_fast "$user_dir/in_90_pre.fasta" -id 0.90 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$user_dir/clusters_90_pre.uc"
     python3 "$script_dir/clusterparser_preclust3_pre.py" "$run_id"
 
     echo "Running 80% clustering..."
     ## 80% clustering
-    "$program_dir/usearch" -cluster_fast "$user_dir/in_80_pre.fasta" -id 0.80 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$user_dir/clusters_80_pre.uc"
+    "$program_dir/usearch" -cluster_fast "$user_dir/in_80_pre.fasta" -id 0.80 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$user_dir/clusters_80_pre.uc"
     python3 "$script_dir/clusterparser_preclust_final_pre.py" "$run_id"
 
     echo "Cleaning up intermediate pre-clustering files..."
@@ -246,7 +246,7 @@ if [ "$include_usearch_05_step" == "yes" ]; then
                 if (( "$result" > "16000" ))
                     then
                         echo "to be split: $line with $result sequences"
-                        "$program_dir/usearch" -cluster_fast $fname -id 0.97 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$clusters_pre_dir/clusters_2_90.uc"
+                        "$program_dir/usearch" -cluster_fast $fname -id 0.97 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$clusters_pre_dir/clusters_2_90.uc"
                         ## cluster into clusters_pre/ClusterX_folder/
                         rm -fr "$clusters_pre_dir/clusters/"$line"_folder"
                         mkdir "$clusters_pre_dir/clusters/"$line"_folder"
@@ -259,8 +259,11 @@ if [ "$include_usearch_05_step" == "yes" ]; then
                         python3 "$script_dir/calc_distm_formatter_90_pre.py" "$run_id" "$line"
                 else
                     echo "sm: $line with $result sequences"
-                    ## calculate usearch distance matrix
-                    python3 "$script_dir/calc_distm_formatter_80_pre.py" "$run_id" "$line"
+                    gunzip $fname
+                    gunzipped_fname=$(echo $fname | sed -e "s/.gz$//")
+                    gunzipped_line=$(echo $line | sed -e "s/.gz$//")
+                    python3 "$script_dir/calc_distm_formatter_80_pre.py" "$run_id" "$gunzipped_line"
+                    gzip -S ".gz" $gunzipped_fname
                 fi
         fi
     done < "$input_95"
@@ -280,7 +283,7 @@ fi
 echo "Finding best matches using vsearch_global..."
 ## Find best matches to user’s sequences in the existing SH sequence dataset using usearch_global algorithm.
 pushd "$user_dir"
-"$program_dir/vsearch/bin/vsearch" --usearch_global "$user_dir/core_reps_pre.fasta" --db "$udb_data_dir/sanger_refs_sh_full.udb" --strand plus --id 0.8 --threads $THREADS --iddef 0 --gapopen 0I/0E --gapext 2I/1E --uc "$user_dir/closedref.80.map.uc" --maxaccepts 3 --maxrejects 0
+"$program_dir/vsearch/bin/vsearch" --usearch_global "$user_dir/core_reps_pre.fasta" --db "$udb_data_dir/sanger_refs_sh_full.udb" --strand plus --id 0.8 --threads $THREADS --iddef 2 --gapopen 1I/0E --gapext 1I/0E --uc "$user_dir/closedref.80.map.uc" --maxaccepts 3 --maxrejects 0
 popd
 echo "Best match search completed."
 
@@ -304,9 +307,9 @@ for nbr1 in {0..9}
     do
         for nbr2 in {0..9}
             do
-                if ls UCL10_0$nbr1$nbr2* 1> /dev/null 2>&1
+                if ls SH0$nbr1$nbr2* 1> /dev/null 2>&1
                     then
-                        ls UCL10_0$nbr1$nbr2* >> "$user_dir/compounds/tmp.txt"
+                        ls SH0$nbr1$nbr2* >> "$user_dir/compounds/tmp.txt"
                 fi
             done
     done
@@ -322,7 +325,7 @@ do
         if (( "$result" > "16000" ))
             then
             echo "to be split: $line with $result sequences"
-            "$program_dir/usearch" -cluster_fast $fname -id 0.97 -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$user_dir/compounds/clusters_2_90.uc"
+            "$program_dir/usearch" -cluster_fast $fname -id 0.97 -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$user_dir/compounds/clusters_2_90.uc"
             ## cluster into usearch_sh/ClusterX_folder/
             rm -fr $user_dir"/compounds/"$line"_folder"
             mkdir $user_dir"/compounds/"$line"_folder"
@@ -336,8 +339,11 @@ do
             python3 $script_dir"/calc_distm_formatter_90.py" "$run_id" "$line"
         else
             echo "sm: $line with $result sequences"
-            # calculate usearch distance matrix and generate (SH) clusters
-            python3 $script_dir"/calc_distm_formatter_80.py" "$run_id" "$line"
+            gunzip $fname
+            gunzipped_fname=$(echo $fname | sed -e "s/.gz$//")
+            gunzipped_line=$(echo $line | sed -e "s/.gz$//")
+            python3 $script_dir"/calc_distm_formatter_80.py" "$run_id" "$gunzipped_line"
+            gzip -S ".gz" $gunzipped_fname
         fi
   fi
 done < "$input_95"
@@ -345,21 +351,21 @@ echo "Compound clusters processing completed."
 
 echo "Parsing usearch output at various thresholds..."
 echo "03"
-python3 "$script_dir/analyse_usearch_output.py" "$run_id" 03
+python3 "$script_dir/analyse_usearch_output_sh.py" "$run_id" 03
 echo "025"
-python3 "$script_dir/analyse_usearch_output.py" "$run_id" 025
+python3 "$script_dir/analyse_usearch_output_sh.py" "$run_id" 025
 echo "02"
-python3 "$script_dir/analyse_usearch_output.py" "$run_id" 02
+python3 "$script_dir/analyse_usearch_output_sh.py" "$run_id" 02
 echo "015"
-python3 "$script_dir/analyse_usearch_output.py" "$run_id" 015
+python3 "$script_dir/analyse_usearch_output_sh.py" "$run_id" 015
 echo "01"
-python3 "$script_dir/analyse_usearch_output.py" "$run_id" 01
+python3 "$script_dir/analyse_usearch_output_sh.py" "$run_id" 01
 echo "005"
-python3 "$script_dir/analyse_usearch_output.py" "$run_id" 005
+python3 "$script_dir/analyse_usearch_output_sh.py" "$run_id" 005
 
 echo "Parsing SH matches..."
 ## parse matches files to output information about input sequences and their belonging to SHs on different thresholds
-perl $script_dir/parse_matches.pl "$run_id"
+perl $script_dir/parse_matches_sh.pl "$run_id"
 
 echo "Checking for NOHITS sequences..."
 ## NOHITS: Run usearch on 4 different thresholds for those sequences that didn’t match any existing SH sequence
@@ -378,22 +384,22 @@ if [ -f "$user_dir/nohits.fasta" ]
 
                 echo "Running NOHITS clustering at 97%..."
                 ## 97%
-                "$program_dir/usearch" -cluster_fast "$user_dir/nohits.fasta" -id 0.97 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -uc "$user_dir/clusters_97.uc"
+                "$program_dir/usearch" -cluster_fast "$user_dir/nohits.fasta" -id 0.97 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -uc "$user_dir/clusters_97.uc"
                 python3 "$script_dir/clusterparser_preclust1.py" "$run_id"
 
                 echo "Running NOHITS clustering at 95%..."
                 ## 95%
-                "$program_dir/usearch" -cluster_fast "$user_dir/in_95.fasta" -id 0.95 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -uc "$user_dir/clusters_95.uc"
+                "$program_dir/usearch" -cluster_fast "$user_dir/in_95.fasta" -id 0.95 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -uc "$user_dir/clusters_95.uc"
                 python3 "$script_dir/clusterparser_preclust2.py" "$run_id"
 
                 echo "Running NOHITS clustering at 90%..."
                 ## 90%
-                "$program_dir/usearch" -cluster_fast "$user_dir/in_90.fasta" -id 0.90 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -uc "$user_dir/clusters_90.uc"
+                "$program_dir/usearch" -cluster_fast "$user_dir/in_90.fasta" -id 0.90 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -uc "$user_dir/clusters_90.uc"
                 python3 "$script_dir/clusterparser_preclust3.py" "$run_id"
 
                 echo "Running NOHITS clustering at 80%..."
                 ## 80%
-                "$program_dir/usearch" -cluster_fast "$user_dir/in_80.fasta" -id 0.80 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -uc "$user_dir/clusters_80.uc"
+                "$program_dir/usearch" -cluster_fast "$user_dir/in_80.fasta" -id 0.80 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -uc "$user_dir/clusters_80.uc"
                 python3 "$script_dir/clusterparser_preclust_final.py" "$run_id"
 
                 echo "Processing NOHITS compound clusters..."
@@ -429,7 +435,7 @@ if [ -f "$user_dir/nohits.fasta" ]
                         if (( "$result" > "16000" ))
                             then
                             echo "to be split: $line with $result sequences"
-                            "$program_dir/usearch" -cluster_fast $fname -id 0.97 -threads $THREADS -gapopen 0.0/0.0E -gapext 1.0/0.5E -sort other -uc "$clusters_dir/clusters_2_90.uc"
+                            "$program_dir/usearch" -cluster_fast $fname -id 0.97 -threads $THREADS -gapopen 1.0I/0.0E -gapext 1.0I/0.0E -sort other -uc "$clusters_dir/clusters_2_90.uc"
                             ## cluster into clusters/ClusterX_folder/
                             rm -fr $clusters_dir"/clusters/"$line"_folder"
                             mkdir $clusters_dir"/clusters/"$line"_folder"
@@ -452,21 +458,21 @@ if [ -f "$user_dir/nohits.fasta" ]
                 echo "Parsing NOHITS blastclust output..."
                 ## parse blastclust output
                 echo "03 (nohits)"
-                python3 $script_dir/analyse_usearch_output_1.py "$run_id" 03
+                python3 $script_dir/analyse_usearch_output_sh.py "$run_id" 03
                 echo "025 (nohits)"
-                python3 $script_dir/analyse_usearch_output_1.py "$run_id" 025
+                python3 $script_dir/analyse_usearch_output_sh.py "$run_id" 025
                 echo "02 (nohits)"
-                python3 $script_dir/analyse_usearch_output_1.py "$run_id" 02
+                python3 $script_dir/analyse_usearch_output_sh.py "$run_id" 02
                 echo "015 (nohits)"
-                python3 $script_dir/analyse_usearch_output_1.py "$run_id" 015
+                python3 $script_dir/analyse_usearch_output_sh.py "$run_id" 015
                 echo "01 (nohits)"
-                python3 $script_dir/analyse_usearch_output_1.py "$run_id" 01
+                python3 $script_dir/analyse_usearch_output_sh.py "$run_id" 01
                 echo "005 (nohits)"
-                python3 $script_dir/analyse_usearch_output_1.py "$run_id" 005
+                python3 $script_dir/analyse_usearch_output_sh.py "$run_id" 005
 
                 echo "Parsing NOHITS matches..."
                 ## parse matches_1 files (nohits) to output information about input sequences and their belonging to new SHs on different thresholds
-                perl $script_dir/parse_matches_1.pl "$run_id"
+                perl $script_dir/parse_matches_sh.pl "$run_id"
             else
                 echo "No NOHITS sequences found."
         fi
